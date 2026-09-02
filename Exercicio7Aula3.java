db01c76aa6c8d9240ba5e25a147eb1ef598b361d
teles-hub <teles.cauers@gmail.com>
2026-09-02 18:40:14 -0300

    Update Exercicio7Aula3.java

--- Exercicio7Aula3.java
+++ Exercicio7Aula3.java
@@ -2,7 +2,7 @@ public class Exercicio7Aula3 {
     public static void main(String[] args) {
         double resultado = Math.pow(3, 30);
         System.out.println((120-30)==resultado);
-        System.out.println(!((20%4)==1)||(9!=9));
+        System.out.println(!((20%4/2)==1)||(9!=9));
         System.out.println((5%2)>3);
         String a = "a";
         String A = "A";
