1759945b5d3cc7a84a8c31300ba9c08455c3ccee
teles-hub <teles.cauers@gmail.com>
2026-09-09 09:42:16 -0300

    Add files via upload

---
+++ pastaAula3/Exercicio9Aula3.java
@@ -0,0 +1,10 @@
+public class Exercicio9Aula3 {
+    public static void main(String[] args) {
+        
+        double lado = 350;
+        double area = Math.pow(lado, 2);
+        System.out.println("A área de um quadrado de lado medindo 350m é: "+area+"m²");
+        
+    }
+}
+//Cauê Teles Rodrigues da Silva
