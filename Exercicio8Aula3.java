4aebff32fee16815c7438fb1a9a83c7d206ee93f
teles-hub <teles.cauers@gmail.com>
2026-09-09 18:19:49 -0300

    Delete Exercicio8Aula3.java

--- Exercicio8Aula3.java
+++
@@ -1,7 +0,0 @@
-public class Exercicio8Aula3 {
-    public static void main(String[] args) {
-        double resultado = ((8.0+7.5+4.5+9)/4);
-        System.out.printf("%.2f", resultado);
-    }
-}
-//Cauê Teles Rodrigues da Silva
